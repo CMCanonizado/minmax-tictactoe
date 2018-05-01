38464dc43e09f8cce739b003bf36b5781659493f
jasarqui <jzarquilita@up.edu.ph>
2018-05-02 02:06:44 +0800

    AGIK TALON UNA ULO

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@
 // U-10L | Week 11 - Min-Max Trees
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import javax.swing.JOptionPane;
 
 public class Main {
     Board board;
@@ -15,6 +17,7 @@ public class Main {
         while(board.checkRunning() == 5){
             if(board.move == 1){ }
             else if(board.move == 2){
+                HashMap<String, Integer> actionValue = new HashMap<String, Integer>();
                 for(String action : getActions(board.config)){
                     newConfig = new char[3][3];
                     for(int i=0; i<3; i++){
@@ -22,16 +25,27 @@ public class Main {
                             newConfig[i][j] = board.config[i][j];
                         }
                     }
-                    if(getMax(newConfig) == 1){ board.applyAction(action); break; }
-                    else if(getMax(newConfig) == 0){ board.applyAction(action); break; }
-                }                    
+                    actionValue.put(action, getMax(newConfig,0));
+                }  
+
+                // get the highest utility
+                int max = -30000;
+                String maxAction = "";
+                for (String action: actionValue.keySet()) {
+                    if (actionValue.get(action) > max) {
+                        max = actionValue.get(action);
+                        maxAction = action;
+                    }
+                    System.out.println(action + " " + actionValue.get(action));
+                }
+                board.applyAction(maxAction);
             }
         }
 
         board.unclick();
-        if(board.checkRunning() == 1) System.out.println("Player 1 won!");
-        else if(board.checkRunning() == -1) System.out.println("Player 2 won!");        
-        else if(board.checkRunning() == 0) System.out.println("It's a draw!");            
+        if(board.checkRunning() == 1) JOptionPane.showMessageDialog(null, "Player 1 won!", "TIC-TAC-TOE", JOptionPane.INFORMATION_MESSAGE);
+        else if(board.checkRunning() == -1) JOptionPane.showMessageDialog(null, "Player 2 won!", "TIC-TAC-TOE", JOptionPane.INFORMATION_MESSAGE);        
+        else if(board.checkRunning() == 0) JOptionPane.showMessageDialog(null, "It's a draw!", "TIC-TAC-TOE", JOptionPane.INFORMATION_MESSAGE);         
     }
 
     public ArrayList<String> getActions(char[][] config1){
@@ -48,7 +62,7 @@ public class Main {
         return actions;
     }
 
-    public int isTerminal(char[][] config1){
+    public String isTerminal(char[][] config1, int utility){
         String row, col, board = "";
 
         // Check horizontally and vertically
@@ -59,36 +73,43 @@ public class Main {
                 col += config1[j][i];
                 board += config1[i][j];
             }
-            if(row.contains("xxx") || col.contains("xxx")) return 1; // Player 1 won
-            else if(row.contains("ooo") || col.contains("ooo")) return -1; // Player 2 won
+            if(row.contains("ooo") || col.contains("ooo")) return "WIN"; // Player 2 won
+            else if(row.contains("xxx") || col.contains("xxx")) return "FAIL"; // Player 1 won
         }
 
         String dia1 = "" + config1[0][0] + config1[1][1] + config1[2][2];
         String dia2 = "" + config1[0][2] + config1[1][1] + config1[2][0];
 
-        if(dia1.contains("xxx") || dia2.contains("xxx")) return 1; // Player 1 won
-        else if(dia1.contains("ooo") || dia2.contains("xxx")) return -1; // Player 2 won
+        if(dia1.contains("ooo") || dia2.contains("ooo")) return "WIN"; // Player 2 won
+        else if(dia1.contains("xxx") || dia2.contains("xxx")) return "FAIL"; // Player 1 won
         
-        if(!board.contains("e")) return 0; // Draw
+        if(!board.contains("e")) return "DRAW"; // Draw
 
-        return 5; // Placeholder for G
+        return "NONE"; // Placeholder for G
     }
 
-    public int value(char[][] configLol, String node){
+    public int value(char[][] configLol, String node, int utility){
         char[][] config1 = new char[3][3];
         for(int i=0; i<3; i++){
             for(int j=0; j<3; j++){
                 config1[i][j] = configLol[i][j];
             }
         }
-        if(isTerminal(config1) != 5) return isTerminal(config1);
-        if(node == "MIN") return getMax(config1);
-        if(node == "MAX") return getMin(config1);   
+        int is_terminal = 0;
+
+        if (isTerminal(config1, utility).equals("WIN")) is_terminal = 1;
+        else if (isTerminal(config1, utility).equals("PRIORITY")) is_terminal = 32000;
+        else if (isTerminal(config1, utility).equals("FAIL")) is_terminal = -1;
+        else if (isTerminal(config1, utility).equals("DRAW")) is_terminal = 0;
+
+        if(isTerminal(config1, utility).equals("NONE")) return utility + is_terminal;
+        if(node.equals("MIN")) return getMax(config1, utility);
+        if(node.equals("MAX")) return getMin(config1, utility);  
         return 0;
     }
 
-    public int getMax(char[][] configLol){
-        int m = -10;
+    public int getMax(char[][] configLol, int utility){
+        int m = -30000;
         char[][] config1 = new char[3][3];
         for(int i=0; i<3; i++){
             for(int j=0; j<3; j++){
@@ -97,14 +118,15 @@ public class Main {
         }
 
         for(String action : getActions(config1)){    
-            int v = value(board.applyAction(config1,action,'o'),"MAX");
+            int v = value(board.applyAction(config1,action,'o'),"MAX", utility);
             if(v > m) m = v; 
         }
+        System.out.println(m);
         return m;
     }
 
-    public int getMin(char[][] configLol){
-        int m = +10;
+    public int getMin(char[][] configLol, int utility){
+        int m = 30000;
         char[][] config1 = new char[3][3];
         for(int i=0; i<3; i++){
             for(int j=0; j<3; j++){
@@ -113,9 +135,10 @@ public class Main {
         }
 
         for(String action : getActions(config1)){
-            int v = value(board.applyAction(config1,action,'x'),"MIN");
+            int v = value(board.applyAction(config1,action,'x'),"MIN", utility);
             if(v < m) m = v; 
         }
+        System.out.println(m);
         return m;
     }
 
